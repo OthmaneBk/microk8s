2426a76db98a3598e400d5585615cd84ddb92272
M-Lahmer <mohammed.lahmer@gmail.com>
2024-12-12 11:43:22 +0100

    k8s

--- init.sql
+++ init.sql
@@ -8,3 +8,7 @@ CREATE TABLE IF NOT EXISTS T_USER(
     email VARCHAR(30) UNIQUE,
     password VARCHAR(30)
 );
+
+INSERT INTO TABLE T_USER ('esisa@esisa.ac.ma', 1234);
+
+INSERT INTO TABLE T_USER ('test@esisa.ac.ma', 1234);
